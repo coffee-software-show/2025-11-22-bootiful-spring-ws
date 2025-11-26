453fb2be36bd67c6fea4cbaf9cc3f4393c1c914a
Josh Long <54473+joshlong@users.noreply.github.com>
2025-11-26 02:34:16 -0800

    yolo

--- client/src/main/java/com/example/client/ClientApplication.java
+++ client/src/main/java/com/example/client/ClientApplication.java
@@ -3,9 +3,9 @@ package com.example.client;
 import com.example.ws.Country;
 import com.example.ws.GetCountryRequest;
 import com.example.ws.GetCountryResponse;
+import jakarta.xml.soap.SOAPException;
 import org.apache.wss4j.dom.WSConstants;
 import org.apache.wss4j.dom.handler.WSHandlerConstants;
-import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
@@ -22,7 +22,6 @@ import org.springframework.http.MediaType;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.context.SecurityContextHolderStrategy;
 import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
@@ -34,22 +33,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestClient;
-import org.springframework.ws.client.WebServiceClientException;
 import org.springframework.ws.client.core.WebServiceTemplate;
 import org.springframework.ws.client.support.interceptor.ClientInterceptor;
 import org.springframework.ws.context.MessageContext;
-import org.springframework.ws.soap.SoapMessage;
+import org.springframework.ws.soap.saaj.SaajSoapMessage;
 import org.springframework.ws.soap.security.wss4j2.Wss4jSecurityInterceptor;
 import org.springframework.xml.transform.StringResult;
-import org.springframework.xml.transform.StringSource;
-import org.springframework.xml.transform.TransformerObjectSupport;
 
-import javax.xml.namespace.QName;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.transform.dom.DOMSource;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.Objects;
+import java.util.UUID;
 
 @SpringBootApplication
 public class ClientApplication {
@@ -113,93 +111,103 @@ class Client2Configuration {
 
 }
 
-/**
- * In this scenario, we want to act as OAuth client. All requests must originate with a
- * valid OAuth token which we can then install in the request made to the downstream SOAP
- * service.
- *
- */
 @Configuration
 class Client3Configuration {
 
 	@Bean
-	OAuthBearerSecurityInterceptor oAuthBearerSecurityInterceptor(
+	JwtBinarySecurityTokenClientInterceptor oAuthBearerSecurityInterceptor(
 			OAuth2AuthorizedClientManager authorizedClientManager) {
-		return new OAuthBearerSecurityInterceptor(authorizedClientManager);
+		return new JwtBinarySecurityTokenClientInterceptor(authorizedClientManager);
 	}
 
-	/**
-	 * injects a custom security header to convey the OAuth token on each request.
-	 */
-	static class OAuthBearerSecurityInterceptor extends TransformerObjectSupport implements ClientInterceptor {
+	static class JwtBinarySecurityTokenClientInterceptor implements ClientInterceptor {
 
 		private static final String WSSE_NS = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd";
 
-		private static final String OAUTH_NS = "http://joshlong.com/soap/security/oauth";
+		private static final String WSU_NS = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd";
+
+		private static final String ENCODING_TYPE = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary";
+
+		private static final String VALUE_TYPE_JWT = "urn:ietf:params:oauth:token-type:jwt";
 
 		private final SecurityContextHolderStrategy securityContextHolderStrategy = SecurityContextHolder
 			.getContextHolderStrategy();
 
 		private final OAuth2AuthorizedClientManager authorizedClientManager;
 
-		OAuthBearerSecurityInterceptor(OAuth2AuthorizedClientManager authorizedClientManager) {
+		JwtBinarySecurityTokenClientInterceptor(OAuth2AuthorizedClientManager authorizedClientManager) {
 			this.authorizedClientManager = authorizedClientManager;
-		}
-
-		@Override
-		public boolean handleRequest(@NonNull MessageContext messageContext) throws WebServiceClientException {
-
-			var soapMessage = (SoapMessage) messageContext.getRequest();
-			var soapHeader = soapMessage.getSoapHeader();
-			var securityNs = new QName(WSSE_NS, "Security", "wsse");
-			var soapHeaderElement = Objects.requireNonNull(soapHeader).addHeaderElement(securityNs);
-			soapHeaderElement.setMustUnderstand(true);
-			var bearerFragment = """
-					<oauth:BearerToken xmlns:oauth="%s">%s</oauth:BearerToken>
-					""".formatted(OAUTH_NS, this.token());
-
-			try {
-				var transformer = this.createTransformer();
-				transformer.transform(new StringSource(bearerFragment), soapHeaderElement.getResult());
-				return true;
-			} //
-			catch (Exception e) {
-				throw new RuntimeException(e);
-			}
-
 		}
 
 		private String token() {
 			var principal = this.securityContextHolderStrategy.getContext().getAuthentication();
 			if (principal instanceof OAuth2AuthenticationToken auth2AuthenticationToken) {
 				var clientRegistrationId = auth2AuthenticationToken.getAuthorizedClientRegistrationId();
-				var oAuth2AuthorizedClient = this.oAuth2AuthorizedClient(clientRegistrationId,
-						auth2AuthenticationToken);
-				return oAuth2AuthorizedClient.getAccessToken().getTokenValue();
+				var authorizeRequest = OAuth2AuthorizeRequest.withClientRegistrationId(clientRegistrationId)
+					.principal(auth2AuthenticationToken)
+					.build();
+				var oAuth2AuthorizedClient = this.authorizedClientManager.authorize(authorizeRequest);
+				return Objects.requireNonNull(oAuth2AuthorizedClient).getAccessToken().getTokenValue();
 			}
 			throw new IllegalStateException("couldn't resolve the registered client id and an associated token!");
 		}
 
-		private OAuth2AuthorizedClient oAuth2AuthorizedClient(String clientRegistrationId, Authentication principal) {
-			var authorizeRequest = OAuth2AuthorizeRequest.withClientRegistrationId(clientRegistrationId)
-				.principal(principal)
-				.build();
-			return this.authorizedClientManager.authorize(authorizeRequest);
+		@Override
+		public boolean handleRequest(MessageContext messageContext) {
+			var message = messageContext.getRequest();
+			if (message instanceof SaajSoapMessage soapMessage) {
+				try {
+					this.addBinarySecurityToken(soapMessage, this.token());
+				}
+				catch (SOAPException e) {
+					throw new RuntimeException("Failed to add BinarySecurityToken to SOAP header", e);
+				}
+			}
+			return true;
+		}
+
+		private void addBinarySecurityToken(SaajSoapMessage saajMessage, String jwt) throws SOAPException {
+			var soapMessage = saajMessage.getSaajMessage();
+			var envelope = soapMessage.getSOAPPart().getEnvelope();
+			var header = envelope.getHeader();
+			if (header == null) {
+				header = envelope.addHeader();
+			}
+
+			// wsse:Security
+			var securityName = envelope.createName("Security", "wsse", WSSE_NS);
+			var securityHeader = header.addHeaderElement(securityName);
+
+			// wsse:BinarySecurityToken
+			var bstName = envelope.createName("BinarySecurityToken", "wsse", WSSE_NS);
+			var bst = securityHeader.addChildElement(bstName);
+
+			// Attributes
+			bst.addAttribute(envelope.createName("ValueType"), VALUE_TYPE_JWT);
+			bst.addAttribute(envelope.createName("EncodingType"), ENCODING_TYPE);
+
+			// wsu:Id attribute
+			var wsuIdName = envelope.createName("Id", "wsu", WSU_NS);
+			bst.addAttribute(wsuIdName, "jwt-" + UUID.randomUUID());
+
+			// Value: base64(jwt bytes) so that on the server side
+			// binarySecurityToken.getToken() returns the original jwt bytes
+			var encoded = Base64.getEncoder().encodeToString(jwt.getBytes(StandardCharsets.UTF_8));
+			bst.addTextNode(encoded);
 		}
 
 		@Override
-		public boolean handleResponse(@NonNull MessageContext messageContext) throws WebServiceClientException {
+		public boolean handleResponse(MessageContext messageContext) {
 			return true;
 		}
 
 		@Override
-		public boolean handleFault(@NonNull MessageContext messageContext) throws WebServiceClientException {
+		public boolean handleFault(MessageContext messageContext) {
 			return true;
 		}
 
 		@Override
-		public void afterCompletion(@NonNull MessageContext messageContext, @Nullable Exception ex)
-				throws WebServiceClientException {
+		public void afterCompletion(MessageContext messageContext, Exception ex) {
 		}
 
 	}
@@ -212,7 +220,7 @@ class Client3Configuration {
 	}
 
 	@Bean
-	WebServiceTemplate webServiceTemplate(OAuthBearerSecurityInterceptor oAuthBearerSecurityInterceptor,
+	WebServiceTemplate webServiceTemplate(JwtBinarySecurityTokenClientInterceptor oAuthBearerSecurityInterceptor,
 			Jaxb2Marshaller jaxb2Marshaller, WebServiceTemplateBuilder builder) {
 		return builder.interceptors(oAuthBearerSecurityInterceptor)
 			.setDefaultUri("http://localhost:8080/ws")
