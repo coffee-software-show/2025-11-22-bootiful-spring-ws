942f25efb515f6a186cd497e2e351df72e1faaf2
Josh Long <54473+joshlong@users.noreply.github.com>
2025-11-28 13:36:48 -0800

    polish

--- ws/src/main/java/com/example/ws/OAuthAuthenticationSecurityConfiguration.java
+++ ws/src/main/java/com/example/ws/OAuthAuthenticationSecurityConfiguration.java
@@ -49,7 +49,8 @@ class OAuthAuthenticationSecurityConfiguration implements WsConfigurer {
 
 	@Bean
 	Customizer<HttpSecurity> httpSecurityCustomizer(@Value("${spring.webservices.path}") String wsPath) {
-		return httpSecurity -> httpSecurity.authorizeHttpRequests(a -> a.requestMatchers(wsPath).permitAll())
+		return httpSecurity -> httpSecurity //
+			.authorizeHttpRequests(a -> a.requestMatchers(wsPath).permitAll()) //
 			.csrf(AbstractHttpConfigurer::disable);
 	}
 
@@ -74,80 +75,81 @@ class OAuthAuthenticationSecurityConfiguration implements WsConfigurer {
 		return new OAuthBinarySecurityTokenEndpointInterceptor(provider);
 	}
 
-}
+	static class OAuthBinarySecurityTokenEndpointInterceptor implements EndpointInterceptor {
 
-class OAuthBinarySecurityTokenEndpointInterceptor implements EndpointInterceptor {
+		private static final String WSSE_NS = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd";
 
-	private static final String WSSE_NS = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd";
+		private static final String VALUE_TYPE_JWT = "urn:ietf:params:oauth:token-type:jwt";
 
-	private static final String VALUE_TYPE_JWT = "urn:ietf:params:oauth:token-type:jwt";
+		private final SecurityContextHolderStrategy securityContextHolderStrategy = SecurityContextHolder
+			.getContextHolderStrategy();
 
-	private final SecurityContextHolderStrategy securityContextHolderStrategy = SecurityContextHolder
-		.getContextHolderStrategy();
+		private final JwtAuthenticationProvider jwtAuthenticationProvider;
 
-	private final JwtAuthenticationProvider jwtAuthenticationProvider;
-
-	OAuthBinarySecurityTokenEndpointInterceptor(JwtAuthenticationProvider jwtAuthenticationProvider) {
-		this.jwtAuthenticationProvider = jwtAuthenticationProvider;
-	}
-
-	@Override
-	public boolean handleRequest(MessageContext messageContext, @NonNull Object endpoint) throws Exception {
-		var request = messageContext.getRequest();
-		try {
-			if (request instanceof SaajSoapMessage soapMessage) {
-				var jwt = this.extractJwtFromBinarySecurityToken(soapMessage);
-				var authenticated = this.jwtAuthenticationProvider
-					.authenticate(new BearerTokenAuthenticationToken(jwt));
-				if (Objects.requireNonNull(authenticated).isAuthenticated()) {
-					this.securityContextHolderStrategy.getContext().setAuthentication(authenticated);
-					return true;
-				}
-			} //
-			throw new IllegalStateException("Authentication is not a valid token");
-		} //
-		catch (Exception e) {
-			return addSoapFault(messageContext, "Invalid or expired JWT: " + e.getMessage());
+		OAuthBinarySecurityTokenEndpointInterceptor(JwtAuthenticationProvider jwtAuthenticationProvider) {
+			this.jwtAuthenticationProvider = jwtAuthenticationProvider;
 		}
-	}
 
-	private String extractJwtFromBinarySecurityToken(SaajSoapMessage saajMessage) throws SOAPException {
-		var soapMessage = saajMessage.getSaajMessage();
-		var header = soapMessage.getSOAPHeader();
-		Assert.notNull(header, "SOAP Header not found");
-		var securityHeaders = header.getChildElements(new QName(WSSE_NS, "Security", "wsse"));
-		Assert.state(securityHeaders.hasNext(), "there are no security headers");
-		var securityHeader = (SOAPHeaderElement) securityHeaders.next();
-		var bstElements = securityHeader.getChildElements(new QName(WSSE_NS, "BinarySecurityToken", "wsse"));
-		Assert.state(bstElements.hasNext(), "there are no binary security tokens");
-		var bst = (SOAPElement) bstElements.next();
-		var valueType = bst.getAttribute("ValueType");
-		Assert.state(VALUE_TYPE_JWT.equals(valueType), "it's not a valid JWT");
-		var base64Encoded = bst.getTextContent();
-		var decoded = Base64.getDecoder().decode(base64Encoded);
-		return new String(decoded, StandardCharsets.UTF_8);
-	}
+		@Override
+		public boolean handleRequest(MessageContext messageContext, @NonNull Object endpoint) throws Exception {
+			var request = messageContext.getRequest();
+			try {
+				if (request instanceof SaajSoapMessage soapMessage) {
+					var jwt = this.extractJwtFromBinarySecurityToken(soapMessage);
+					var authenticated = this.jwtAuthenticationProvider
+						.authenticate(new BearerTokenAuthenticationToken(jwt));
+					if (Objects.requireNonNull(authenticated).isAuthenticated()) {
+						this.securityContextHolderStrategy.getContext().setAuthentication(authenticated);
+						return true;
+					}
+				} //
+				throw new IllegalStateException("Authentication is not a valid token");
+			} //
+			catch (Exception e) {
+				return addSoapFault(messageContext, "Invalid or expired JWT: " + e.getMessage());
+			}
+		}
 
-	private boolean addSoapFault(MessageContext messageContext, String reason) {
-		var response = (SoapMessage) messageContext.getResponse();
-		response.getSoapBody().addClientOrSenderFault(reason, Locale.ENGLISH);
-		return false;
-	}
+		private String extractJwtFromBinarySecurityToken(SaajSoapMessage saajMessage) throws SOAPException {
+			var soapMessage = saajMessage.getSaajMessage();
+			var header = soapMessage.getSOAPHeader();
+			Assert.notNull(header, "SOAP Header not found");
+			var securityHeaders = header.getChildElements(new QName(WSSE_NS, "Security", "wsse"));
+			Assert.state(securityHeaders.hasNext(), "there are no security headers");
+			var securityHeader = (SOAPHeaderElement) securityHeaders.next();
+			var bstElements = securityHeader.getChildElements(new QName(WSSE_NS, "BinarySecurityToken", "wsse"));
+			Assert.state(bstElements.hasNext(), "there are no binary security tokens");
+			var bst = (SOAPElement) bstElements.next();
+			var valueType = bst.getAttribute("ValueType");
+			Assert.state(VALUE_TYPE_JWT.equals(valueType), "it's not a valid JWT");
+			var base64Encoded = bst.getTextContent();
+			var decoded = Base64.getDecoder().decode(base64Encoded);
+			return new String(decoded, StandardCharsets.UTF_8);
+		}
 
-	@Override
-	public boolean handleResponse(@NonNull MessageContext messageContext, @NonNull Object endpoint) throws Exception {
-		return true;
-	}
+		private boolean addSoapFault(MessageContext messageContext, String reason) {
+			var response = (SoapMessage) messageContext.getResponse();
+			response.getSoapBody().addClientOrSenderFault(reason, Locale.ENGLISH);
+			return false;
+		}
 
-	@Override
-	public boolean handleFault(@NonNull MessageContext messageContext, @NonNull Object endpoint) throws Exception {
-		return true;
-	}
+		@Override
+		public boolean handleResponse(@NonNull MessageContext messageContext, @NonNull Object endpoint)
+				throws Exception {
+			return true;
+		}
 
-	@Override
-	public void afterCompletion(@NonNull MessageContext messageContext, @NonNull Object endpoint, Exception ex)
-			throws Exception {
-		SecurityContextHolder.clearContext();
+		@Override
+		public boolean handleFault(@NonNull MessageContext messageContext, @NonNull Object endpoint) throws Exception {
+			return true;
+		}
+
+		@Override
+		public void afterCompletion(@NonNull MessageContext messageContext, @NonNull Object endpoint, Exception ex)
+				throws Exception {
+			SecurityContextHolder.clearContext();
+		}
+
 	}
 
 }
