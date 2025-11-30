e6e8d7299ed5a8059fdef4175ca7cc906b0e55c7
Josh Long <54473+joshlong@users.noreply.github.com>
2025-11-29 17:36:19 -0800

    simplified

--- client/src/main/java/com/example/client/ClientApplication.java
+++ client/src/main/java/com/example/client/ClientApplication.java
@@ -1,7 +1,9 @@
 package com.example.client;
 
-import com.example.ws.*;
+import com.example.ws.MessageRequest;
+import com.example.ws.MessageResponse;
 import jakarta.xml.soap.SOAPException;
+import org.apache.wss4j.dom.WSConstants;
 import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
 import org.springframework.aot.hint.MemberCategory;
@@ -27,181 +29,131 @@ import org.springframework.ws.client.core.WebServiceTemplate;
 import org.springframework.ws.client.support.interceptor.ClientInterceptor;
 import org.springframework.ws.context.MessageContext;
 import org.springframework.ws.soap.saaj.SaajSoapMessage;
-import org.springframework.xml.transform.StringResult;
 
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.dom.DOMSource;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.Objects;
-import java.util.UUID;
-
 
 @SpringBootApplication
 @ImportRuntimeHints(ClientApplication.Hints.class)
 public class ClientApplication {
 
-    static class Hints implements RuntimeHintsRegistrar {
+	static class Hints implements RuntimeHintsRegistrar {
 
-        @Override
-        public void registerHints(@NonNull RuntimeHints hints, @Nullable ClassLoader classLoader) {
-            for (var t : JAXB_CLASSES)
-                hints.reflection().registerType(t, MemberCategory.values());
-        }
-    }
+		@Override
+		public void registerHints(@NonNull RuntimeHints hints, @Nullable ClassLoader classLoader) {
+			for (var t : JAXB_CLASSES)
+				hints.reflection().registerType(t, MemberCategory.values());
+		}
 
-    public static void main(String[] args) {
-        SpringApplication.run(ClientApplication.class, args);
-    }
+	}
 
-    static final Class<?>[] JAXB_CLASSES = new Class<?>[]{
-            GetCountryResponse.class,
-            GetCountryRequest.class,
-            Currency.class,
-            GetMeResponse.class,
-            Country.class
-    };
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApplication.class, args);
+	}
 
-    @Bean
-    Jaxb2Marshaller jaxb2Marshaller() {
-        var marshaller = new Jaxb2Marshaller();
-        marshaller.setClassesToBeBound(JAXB_CLASSES);
-        return marshaller;
-    }
+	static final Class<?>[] JAXB_CLASSES = new Class<?>[] { MessageResponse.class, MessageRequest.class, };
 
-    @Bean
-    WebServiceTemplate webServiceTemplate(
-            Jaxb2Marshaller marshaller,
-            OAuthClientInterceptor interceptor,
-            WebServiceTemplateBuilder builder) {
-        return builder
-                .interceptors(interceptor)
-                .setMarshaller(marshaller)
-                .setUnmarshaller(marshaller)
-                .setDefaultUri("http://localhost:8080/ws")
-                .build();
-    }
+	@Bean
+	Jaxb2Marshaller jaxb2Marshaller() {
+		var marshaller = new Jaxb2Marshaller();
+		marshaller.setClassesToBeBound(JAXB_CLASSES);
+		return marshaller;
+	}
+
+	@Bean
+	WebServiceTemplate webServiceTemplate(
+
+			Jaxb2Marshaller marshaller, ClientInterceptor[] interceptors, WebServiceTemplateBuilder builder) {
+		return builder ///
+			.interceptors(interceptors) //
+			.setMarshaller(marshaller)
+			.setUnmarshaller(marshaller)
+			.setDefaultUri("http://localhost:8080/ws")
+			.build();
+	}
+
 }
 
 @Component
-class OAuthClientInterceptor implements ClientInterceptor {
+class OAuthTokenClientInterceptor implements ClientInterceptor {
 
-    private static final String WSSE_NS = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd";
+	private final SecurityContextHolderStrategy strategy = SecurityContextHolder.getContextHolderStrategy();
 
-    private static final String WSU_NS = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd";
+	private final OAuth2AuthorizedClientManager authorizedClientManager;
 
-    private static final String ENCODING_TYPE = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary";
+	OAuthTokenClientInterceptor(OAuth2AuthorizedClientManager authorizedClientManager) {
+		this.authorizedClientManager = authorizedClientManager;
+	}
 
-    private static final String VALUE_TYPE_JWT = "urn:ietf:params:oauth:token-type:jwt";
+	@Override
+	public boolean handleRequest(@NonNull MessageContext messageContext) throws WebServiceClientException {
+		try {
+			var jwt = this.jwt();
+			if (messageContext.getRequest() instanceof SaajSoapMessage sm) {
+				var env = sm.getSaajMessage().getSOAPPart().getEnvelope();
+				if (env.getHeader() == null)
+					env.addHeader();
+				var security = env.getHeader().addHeaderElement(env.createName("Security", WSConstants.WSSE_NS));
+				var bst = security.addChildElement(env.createName("BinarySecurityToken", WSConstants.WSSE_NS));
+				bst.setTextContent(Base64.getEncoder()
+					.encodeToString(Objects.requireNonNull(jwt).getBytes(StandardCharsets.UTF_8)));
+			}
+		} //
+		catch (SOAPException e) {
+			throw new RuntimeException(e);
+		}
+		return true;
+	}
 
-    private final SecurityContextHolderStrategy securityContextHolderStrategy =
-            SecurityContextHolder.getContextHolderStrategy();
+	private String jwt() {
+		var auth = this.strategy.getContext().getAuthentication();
+		if (auth instanceof OAuth2AuthenticationToken oAuth2AuthenticationToken) {
+			var request = OAuth2AuthorizeRequest
+				.withClientRegistrationId(oAuth2AuthenticationToken.getAuthorizedClientRegistrationId())
+				.principal(auth)
+				.build();
+			var authorized = this.authorizedClientManager.authorize(request);
+			if (authorized != null) {
+				return authorized.getAccessToken().getTokenValue();
+			}
+		}
+		return null;
+	}
 
-    private final OAuth2AuthorizedClientManager authorizedClientManager;
+	@Override
+	public boolean handleResponse(@NonNull MessageContext messageContext) throws WebServiceClientException {
+		return true;
+	}
 
-    OAuthClientInterceptor(OAuth2AuthorizedClientManager authorizedClientManager) {
-        this.authorizedClientManager = authorizedClientManager;
-    }
+	@Override
+	public boolean handleFault(@NonNull MessageContext messageContext) throws WebServiceClientException {
+		return true;
+	}
 
-    @Override
-    public boolean handleRequest(@NonNull MessageContext messageContext) throws WebServiceClientException {
-        var principal = this.securityContextHolderStrategy.getContext().getAuthentication();
-        if (principal instanceof OAuth2AuthenticationToken oAuth2AuthenticationToken
-                && messageContext.getRequest() instanceof SaajSoapMessage saajSoapMessage
-        ) {
-            var registeredClient = oAuth2AuthenticationToken.getAuthorizedClientRegistrationId();
-            var token = OAuth2AuthorizeRequest
-                    .withClientRegistrationId(registeredClient)
-                    .principal(principal)
-                    .build();
-            var authorize = this.authorizedClientManager.authorize(token);
-            var accessToken = Objects.requireNonNull(authorize).getAccessToken();
-            var jwt = accessToken.getTokenValue();
-            try {
-                this.addBinarySecurityToken(saajSoapMessage, jwt);
-                return true;
-            } //
-            catch (SOAPException e) {
-                throw new RuntimeException(e);
-            }
-            // todo add to outgoing request destined for Spring WS endpoint on :8080
-        }
-        return false;
-    }
+	@Override
+	public void afterCompletion(@NonNull MessageContext messageContext, @Nullable Exception ex)
+			throws WebServiceClientException {
 
+	}
 
-    private void addBinarySecurityToken(SaajSoapMessage saajMessage, String jwt) throws SOAPException {
-        var soapMessage = saajMessage.getSaajMessage();
-        var envelope = soapMessage.getSOAPPart().getEnvelope();
-        var header = envelope.getHeader();
-        if (header == null) {
-            header = envelope.addHeader();
-        }
-
-        // wsse:Security
-        var securityName = envelope.createName("Security", "wsse", WSSE_NS);
-        var securityHeader = header.addHeaderElement(securityName);
-
-        // wsse:BinarySecurityToken
-        var bstName = envelope.createName("BinarySecurityToken", "wsse", WSSE_NS);
-        var bst = securityHeader.addChildElement(bstName);
-
-        // Attributes
-        bst.addAttribute(envelope.createName("ValueType"), VALUE_TYPE_JWT);
-        bst.addAttribute(envelope.createName("EncodingType"), ENCODING_TYPE);
-
-        // wsu:Id attribute
-        var wsuIdName = envelope.createName("Id", "wsu", WSU_NS);
-        bst.addAttribute(wsuIdName, "jwt-" + UUID.randomUUID());
-
-        // Value: base64(jwt bytes) so that on the server side
-        // binarySecurityToken.getToken() returns the original jwt bytes
-        var encoded = Base64.getEncoder().encodeToString(jwt.getBytes(StandardCharsets.UTF_8));
-        bst.addTextNode(encoded);
-    }
-
-
-    @Override
-    public boolean handleResponse(@NonNull MessageContext messageContext) throws WebServiceClientException {
-        return true;
-    }
-
-    @Override
-    public boolean handleFault(@NonNull MessageContext messageContext) throws WebServiceClientException {
-        return true;
-    }
-
-    @Override
-    public void afterCompletion(@NonNull MessageContext messageContext, @Nullable Exception ex) throws WebServiceClientException {
-
-    }
 }
 
 @Controller
 @ResponseBody
 class ClientController {
 
-    private final WebServiceTemplate ws;
+	private final WebServiceTemplate ws;
 
-    ClientController(WebServiceTemplate ws) {
-        this.ws = ws;
-    }
+	ClientController(WebServiceTemplate ws) {
+		this.ws = ws;
+	}
 
-    @GetMapping("/me")
-    String me() throws Exception {
-        var document = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
-        var element = document.createElementNS("http://example.com/ws", "getMeRequest");
-        var source = new DOMSource(element);
-        var result = new StringResult();
-        this.ws.sendSourceAndReceiveToResult(source, result);
-        return result.toString();
-    }
+	@GetMapping("/message")
+	MessageResponse message() {
+		var message = new MessageRequest();
+		message.setName("Bob");
+		return (MessageResponse) this.ws.marshalSendAndReceive(message);
+	}
 
-    @GetMapping("/country")
-    GetCountryResponse country() {
-        var getCountryRequest = new GetCountryRequest();
-        getCountryRequest.setName("United Kingdom");
-        return (GetCountryResponse)
-                this.ws.marshalSendAndReceive(getCountryRequest);
-    }
 }
--- ws/src/main/java/com/example/ws/WsApplication.java
+++ ws/src/main/java/com/example/ws/WsApplication.java
@@ -1,8 +1,6 @@
 package com.example.ws;
 
-import jakarta.xml.soap.SOAPElement;
 import jakarta.xml.soap.SOAPException;
-import jakarta.xml.soap.SOAPHeaderElement;
 import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
 import org.springframework.beans.factory.ObjectProvider;
@@ -15,6 +13,8 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.context.SecurityContextHolderStrategy;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
@@ -31,159 +31,132 @@ import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 import org.springframework.ws.soap.saaj.SaajSoapMessage;
 
-import javax.xml.namespace.QName;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
-import java.util.concurrent.ConcurrentHashMap;
 
 @SpringBootApplication
 public class WsApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(WsApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(WsApplication.class, args);
+	}
 
-    static final String NS = "http://example.com/ws";
+	static final String NS = "http://example.com/ws";
+
 }
-
 
 @Component
 class OAuthTokenInterceptor implements EndpointInterceptor {
 
-    private final SecurityContextHolderStrategy securityContextHolder = SecurityContextHolder.getContextHolderStrategy();
+	private final SecurityContextHolderStrategy securityContextHolder = SecurityContextHolder
+		.getContextHolderStrategy();
 
-    private static final String WSSE_NS = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd";
+	private final AuthenticationProvider authenticationProvider;
 
-    private final AuthenticationProvider authenticationProvider;
+	OAuthTokenInterceptor(AuthenticationProvider authenticationProvider) {
+		this.authenticationProvider = authenticationProvider;
+	}
 
-    OAuthTokenInterceptor(AuthenticationProvider authenticationProvider) {
-        this.authenticationProvider = authenticationProvider;
-    }
+	@Override
+	public boolean handleRequest(@NonNull MessageContext messageContext, @NonNull Object endpoint) throws Exception {
+		if (messageContext.getRequest() instanceof SaajSoapMessage soapMessage) {
+			var token = this.jwt(soapMessage);
+			var bearerAuthentication = new BearerTokenAuthenticationToken(token);
+			var authenticated = Objects.requireNonNull(this.authenticationProvider.authenticate(bearerAuthentication));
+			if (authenticated.isAuthenticated()) {
+				this.securityContextHolder.getContext().setAuthentication(authenticated);
+				return true;
+			}
+		}
+		return false;
+	}
 
-    private String extractJwtToken(SaajSoapMessage saajMessage) throws SOAPException {
-        var soapMessage = saajMessage.getSaajMessage();
-        var header = soapMessage.getSOAPHeader();
-        var securityHeaders = header.getChildElements(new QName(WSSE_NS, "Security", "wsse"));
-        var securityHeader = (SOAPHeaderElement) securityHeaders.next();
-        var bstElements = securityHeader.getChildElements(new QName(WSSE_NS, "BinarySecurityToken", "wsse"));
-        var bst = (SOAPElement) bstElements.next();
-        var base64Encoded = bst.getTextContent();
-        var decoded = Base64.getDecoder().decode(base64Encoded);
-        return new String(decoded, StandardCharsets.UTF_8);
-    }
+	private String jwt(SaajSoapMessage saajMessage) throws SOAPException {
+		var soapMessage = saajMessage.getSaajMessage();
+		var header = soapMessage.getSOAPHeader();
+		var soapHeaderElementIterator = header.examineAllHeaderElements();
+		while (soapHeaderElementIterator.hasNext()) {
+			var she = soapHeaderElementIterator.next();
+			if (she.getLocalName().equals("Security")) {
+				var bstNodes = she.getChildElements();
+				while (bstNodes.hasNext()) {
+					var node = bstNodes.next();
+					if (node.getLocalName().equals("BinarySecurityToken")) {
+						var txt = node.getTextContent();
+						var decoded = Base64.getDecoder().decode(txt);
+						return new String(decoded, StandardCharsets.UTF_8);
+					}
+				}
+			}
+		}
+		throw new IllegalStateException("could not load the JWT token");
+	}
 
-    @Override
-    public boolean handleRequest(@NonNull MessageContext messageContext, @NonNull Object endpoint) throws Exception {
-        if (messageContext.getRequest() instanceof SaajSoapMessage soapMessage) {
-            var token = this.extractJwtToken(soapMessage);
-            var bearerAuthentication = new BearerTokenAuthenticationToken(token);
-            var authenticated = Objects.requireNonNull(this.authenticationProvider.authenticate(bearerAuthentication));
-            if (authenticated.isAuthenticated()) {
-                this.securityContextHolder.getContext().setAuthentication(authenticated);
-                return true;
-            }
-        }
-        return false;
-    }
+	@Override
+	public boolean handleResponse(@NonNull MessageContext messageContext, @NonNull Object endpoint) throws Exception {
+		return true;
+	}
 
+	@Override
+	public boolean handleFault(@NonNull MessageContext messageContext, @NonNull Object endpoint) throws Exception {
+		return true;
+	}
 
-    @Override
-    public boolean handleResponse(@NonNull MessageContext messageContext, @NonNull Object endpoint) throws Exception {
-        return true;
-    }
+	@Override
+	public void afterCompletion(@NonNull MessageContext messageContext, @NonNull Object endpoint,
+			@Nullable Exception ex) throws Exception {
+		this.securityContextHolder.clearContext();
+	}
 
-    @Override
-    public boolean handleFault(@NonNull MessageContext messageContext, @NonNull Object endpoint) throws Exception {
-        return true;
-    }
-
-    @Override
-    public void afterCompletion(@NonNull MessageContext messageContext, @NonNull Object endpoint, @Nullable Exception ex) throws Exception {
-        this.securityContextHolder.clearContext();
-    }
 }
-
 
 @Configuration
-class SecurityConfiguration
-        implements WsConfigurer {
+class SecurityConfiguration implements WsConfigurer {
 
-    private final ObjectProvider<@NonNull OAuthTokenInterceptor> oAuthTokenInterceptors;
+	private final ObjectProvider<@NonNull OAuthTokenInterceptor> oAuthTokenInterceptors;
 
-    SecurityConfiguration(ObjectProvider<@NonNull OAuthTokenInterceptor> oAuthTokenInterceptors) {
-        this.oAuthTokenInterceptors = oAuthTokenInterceptors;
-    }
+	SecurityConfiguration(ObjectProvider<@NonNull OAuthTokenInterceptor> oAuthTokenInterceptors) {
+		this.oAuthTokenInterceptors = oAuthTokenInterceptors;
+	}
 
-    @Override
-    public void addInterceptors(@NonNull List<EndpointInterceptor> interceptors) {
-        interceptors.add(oAuthTokenInterceptors.getIfAvailable());
-    }
+	@Override
+	public void addInterceptors(@NonNull List<EndpointInterceptor> interceptors) {
+		interceptors.add(oAuthTokenInterceptors.getIfAvailable());
+	}
 
-    @Bean
-    JwtDecoder jwtDecoder(@Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}") String issuerUri) {
-        return NimbusJwtDecoder.withIssuerLocation(issuerUri).build();
-    }
+	@Bean
+	JwtDecoder jwtDecoder(@Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}") String issuerUri) {
+		return NimbusJwtDecoder.withIssuerLocation(issuerUri).build();
+	}
 
-    @Bean
-    JwtAuthenticationProvider jwtAuthenticationProvider(JwtDecoder decoder) {
-        return new JwtAuthenticationProvider(decoder);
-    }
+	@Bean
+	JwtAuthenticationProvider jwtAuthenticationProvider(JwtDecoder decoder) {
+		return new JwtAuthenticationProvider(decoder);
+	}
 
-    @Bean
-    Customizer<HttpSecurity> httpSecurityCustomizer(@Value("${spring.webservices.path}") String path) {
-        return http -> http
-                .authorizeHttpRequests(a -> a.requestMatchers(path).permitAll()) //
-                .csrf(AbstractHttpConfigurer::disable);
-    }
+	@Bean
+	Customizer<HttpSecurity> httpSecurityCustomizer(@Value("${spring.webservices.path}") String path) {
+		return http -> http.authorizeHttpRequests(a -> a.requestMatchers(path).permitAll()) //
+			.csrf(AbstractHttpConfigurer::disable);
+	}
+
 }
 
 @Endpoint
-class MeEndpoint {
+class MessageEndpoint {
 
-    private final SecurityContextHolderStrategy securityContextHolderStrategy =
-            SecurityContextHolder.getContextHolderStrategy();
+	private final SecurityContextHolderStrategy securityContextHolderStrategy = SecurityContextHolder
+		.getContextHolderStrategy();
 
-    @ResponsePayload
-    @PayloadRoot(namespace = WsApplication.NS, localPart = "getMeRequest")
-    GetMeResponse getMeResponse() {
-        var authentication = this.securityContextHolderStrategy.getContext().getAuthentication();
-        var gmr = new GetMeResponse();
-        gmr.setName(authentication.getName());
-        return gmr;
-    }
-}
+	@ResponsePayload
+	@PayloadRoot(namespace = WsApplication.NS, localPart = "messageRequest")
+	MessageResponse message(@RequestPayload MessageRequest request) {
+		var authentication = this.securityContextHolderStrategy.getContext().getAuthentication();
+		var mr = new MessageResponse();
+		mr.setMessage("hello, " + Objects.requireNonNull(authentication).getName());
+		return mr;
+	}
 
-@Endpoint
-class CountryEndpoint {
-
-    private final Map<String, Country> countries = new ConcurrentHashMap<>();
-
-    CountryEndpoint() {
-        add("Spain", "Madrid", Currency.EUR);
-        add("France", "Paris", Currency.EUR);
-        add("Poland", "Warsaw", Currency.PLN);
-        add("United Kingdom", "London", Currency.GBP);
-    }
-
-    @ResponsePayload
-    @PayloadRoot(namespace = WsApplication.NS, localPart = "getCountryRequest")
-    GetCountryResponse country(@RequestPayload GetCountryRequest request) {
-        var response = this.countries.get(request.getName());
-        var result = new GetCountryResponse();
-        result.setCountry(response);
-        return result;
-    }
-
-    private void add(String name, String capital, Currency currency) {
-        this.countries.computeIfAbsent(name, _ -> {
-            var c = new Country();
-            c.setName(name);
-            c.setCapital(capital);
-            c.setCurrency(currency);
-            return c;
-        });
-    }
 }
