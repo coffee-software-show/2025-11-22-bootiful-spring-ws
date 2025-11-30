10bd3f11223eaf3bb66057aec3b1adfd1247e2b1
Josh Long <54473+joshlong@users.noreply.github.com>
2025-11-29 16:46:08 -0800

    simplified

--- client/src/main/java/com/example/client/ClientApplication.java
+++ client/src/main/java/com/example/client/ClientApplication.java
@@ -38,125 +38,120 @@ import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.Objects;
 
-
 @SpringBootApplication
 @ImportRuntimeHints(ClientApplication.Hints.class)
 public class ClientApplication {
 
-    static class Hints implements RuntimeHintsRegistrar {
+	static class Hints implements RuntimeHintsRegistrar {
 
-        @Override
-        public void registerHints(@NonNull RuntimeHints hints, @Nullable ClassLoader classLoader) {
-            for (var t : JAXB_CLASSES)
-                hints.reflection().registerType(t, MemberCategory.values());
-        }
-    }
+		@Override
+		public void registerHints(@NonNull RuntimeHints hints, @Nullable ClassLoader classLoader) {
+			for (var t : JAXB_CLASSES)
+				hints.reflection().registerType(t, MemberCategory.values());
+		}
 
-    public static void main(String[] args) {
-        SpringApplication.run(ClientApplication.class, args);
-    }
+	}
 
-    static final Class<?>[] JAXB_CLASSES = new Class<?>[]{
-            MessageResponse.class,
-            MessageRequest.class,
-    };
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApplication.class, args);
+	}
 
-    @Bean
-    Jaxb2Marshaller jaxb2Marshaller() {
-        var marshaller = new Jaxb2Marshaller();
-        marshaller.setClassesToBeBound(JAXB_CLASSES);
-        return marshaller;
-    }
+	static final Class<?>[] JAXB_CLASSES = new Class<?>[] { MessageResponse.class, MessageRequest.class, };
 
-    @Bean
-    WebServiceTemplate webServiceTemplate(
-            Jaxb2Marshaller marshaller,
-            OAuthClientInterceptor interceptor,
-            WebServiceTemplateBuilder builder) {
-        return builder
-                .interceptors(interceptor)
-                .setMarshaller(marshaller)
-                .setUnmarshaller(marshaller)
-                .setDefaultUri("http://localhost:8080/ws")
-                .build();
-    }
+	@Bean
+	Jaxb2Marshaller jaxb2Marshaller() {
+		var marshaller = new Jaxb2Marshaller();
+		marshaller.setClassesToBeBound(JAXB_CLASSES);
+		return marshaller;
+	}
+
+	@Bean
+	WebServiceTemplate webServiceTemplate(Jaxb2Marshaller marshaller, OAuthClientInterceptor interceptor,
+			WebServiceTemplateBuilder builder) {
+		return builder.interceptors(interceptor)
+			.setMarshaller(marshaller)
+			.setUnmarshaller(marshaller)
+			.setDefaultUri("http://localhost:8080/ws")
+			.build();
+	}
+
 }
-
 
 @Component
 class OAuthClientInterceptor implements ClientInterceptor {
 
-    private final SecurityContextHolderStrategy securityContextHolderStrategy =
-            SecurityContextHolder.getContextHolderStrategy();
+	private final SecurityContextHolderStrategy securityContextHolderStrategy = SecurityContextHolder
+		.getContextHolderStrategy();
 
-    private final OAuth2AuthorizedClientManager authorizedClientManager;
+	private final OAuth2AuthorizedClientManager authorizedClientManager;
 
-    OAuthClientInterceptor(OAuth2AuthorizedClientManager authorizedClientManager) {
-        this.authorizedClientManager = authorizedClientManager;
-    }
+	OAuthClientInterceptor(OAuth2AuthorizedClientManager authorizedClientManager) {
+		this.authorizedClientManager = authorizedClientManager;
+	}
 
-    @Override
-    public boolean handleRequest(@NonNull MessageContext messageContext) throws WebServiceClientException {
-        var principal = this.securityContextHolderStrategy.getContext().getAuthentication();
-        if (principal instanceof OAuth2AuthenticationToken oAuth2AuthenticationToken
-                && messageContext.getRequest() instanceof SaajSoapMessage saajSoapMessage
-        ) {
-            var registeredClient = oAuth2AuthenticationToken.getAuthorizedClientRegistrationId();
-            var token = OAuth2AuthorizeRequest
-                    .withClientRegistrationId(registeredClient)
-                    .principal(principal)
-                    .build();
-            var authorize = this.authorizedClientManager.authorize(token);
-            var accessToken = Objects.requireNonNull(authorize).getAccessToken();
-            var jwt = accessToken.getTokenValue();
-            try {
-                var soapMessage = saajSoapMessage.getSaajMessage();
-                var envelope = soapMessage.getSOAPPart().getEnvelope();
-                var header = envelope.getHeader();
-                if (header == null)
-                    header = envelope.addHeader();
-                var securityHeader = header.addHeaderElement(envelope.createName("Security", "wsse", WSConstants.WSSE_NS));
-                var binarySecurityToken = securityHeader.addChildElement(envelope.createName("BinarySecurityToken", "wsse", WSConstants.WSSE_NS));
-                var encoded = Base64.getEncoder().encodeToString(jwt.getBytes(StandardCharsets.UTF_8));
-                binarySecurityToken.addTextNode(encoded);
-                return true;
-            } //
-            catch (SOAPException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return false;
-    }
+	@Override
+	public boolean handleRequest(@NonNull MessageContext messageContext) throws WebServiceClientException {
+		var principal = this.securityContextHolderStrategy.getContext().getAuthentication();
+		if (principal instanceof OAuth2AuthenticationToken oAuth2AuthenticationToken
+				&& messageContext.getRequest() instanceof SaajSoapMessage saajSoapMessage) {
+			var registeredClient = oAuth2AuthenticationToken.getAuthorizedClientRegistrationId();
+			var token = OAuth2AuthorizeRequest.withClientRegistrationId(registeredClient).principal(principal).build();
+			var authorize = this.authorizedClientManager.authorize(token);
+			var accessToken = Objects.requireNonNull(authorize).getAccessToken();
+			var jwt = accessToken.getTokenValue();
+			try {
+				var soapMessage = saajSoapMessage.getSaajMessage();
+				var envelope = soapMessage.getSOAPPart().getEnvelope();
+				var header = envelope.getHeader();
+				if (header == null)
+					header = envelope.addHeader();
+				var securityHeader = header
+					.addHeaderElement(envelope.createName("Security", "wsse", WSConstants.WSSE_NS));
+				var binarySecurityToken = securityHeader
+					.addChildElement(envelope.createName("BinarySecurityToken", "wsse", WSConstants.WSSE_NS));
+				var encoded = Base64.getEncoder().encodeToString(jwt.getBytes(StandardCharsets.UTF_8));
+				binarySecurityToken.addTextNode(encoded);
+				return true;
+			} //
+			catch (SOAPException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return false;
+	}
 
-    @Override
-    public boolean handleResponse(@NonNull MessageContext messageContext) throws WebServiceClientException {
-        return true;
-    }
+	@Override
+	public boolean handleResponse(@NonNull MessageContext messageContext) throws WebServiceClientException {
+		return true;
+	}
 
-    @Override
-    public boolean handleFault(@NonNull MessageContext messageContext) throws WebServiceClientException {
-        return true;
-    }
+	@Override
+	public boolean handleFault(@NonNull MessageContext messageContext) throws WebServiceClientException {
+		return true;
+	}
 
-    @Override
-    public void afterCompletion(@NonNull MessageContext messageContext, @Nullable Exception ex) throws WebServiceClientException {
-    }
+	@Override
+	public void afterCompletion(@NonNull MessageContext messageContext, @Nullable Exception ex)
+			throws WebServiceClientException {
+	}
+
 }
 
 @Controller
 @ResponseBody
 class ClientController {
 
-    private final WebServiceTemplate ws;
+	private final WebServiceTemplate ws;
 
-    ClientController(WebServiceTemplate ws) {
-        this.ws = ws;
-    }
+	ClientController(WebServiceTemplate ws) {
+		this.ws = ws;
+	}
 
-    @GetMapping("/message")
-    MessageResponse message() {
-        var message = new MessageRequest();
-        message.setName("Bob");
-        return (MessageResponse) this.ws.marshalSendAndReceive(message);
-    }
+	@GetMapping("/message")
+	MessageResponse message() {
+		var message = new MessageRequest();
+		message.setName("Bob");
+		return (MessageResponse) this.ws.marshalSendAndReceive(message);
+	}
+
 }
